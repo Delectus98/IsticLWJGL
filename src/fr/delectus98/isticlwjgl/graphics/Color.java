339653e9b197f0d4661c05b5792565585b6379a6
Maxtwist35 <beuchermaxime@laposte.net>
2019-09-07 18:02:19 +0200

    Add constructors with integer values for RGBA value using 0-255 interval converted to 0-1

--- src/fr/delectus98/isticlwjgl/graphics/Color.java
+++ src/fr/delectus98/isticlwjgl/graphics/Color.java
@@ -32,15 +32,42 @@ public final class Color extends ConstColor {
         this.a = a;
     }
 
+    /**
+     * Generating OpenGL Color
+     * @param r Red value [must be between 0 and 255]
+     * @param g Green value [must be between 0 and 255]
+     * @param b Blue value [must be between 0 and 255]
+     */
+    public Color(int r, int g, int b){
+        this.r = r / 255.f;
+        this.g = g / 255.f;
+        this.b = b / 255.f;
+        this.a = 1.f;
+    }
+
+    /**
+     * Generating OpenGL Color
+     * @param r Red value [must be between 0 and 255]
+     * @param g Green value [must be between 0 and 255]
+     * @param b Blue value [must be between 0 and 255]
+     * @param a Opacity value [must be between 0 and 255]
+     */
+    public Color(int r, int g, int b, int a){
+        this.r = r / 255.f;
+        this.g = g / 255.f;
+        this.b = b / 255.f;
+        this.a = a / 255.f;
+    }
+
     // Default colors
-    public static final ConstColor Blue = new Color(0,0,1,1);
-    public static final ConstColor Green = new Color(0,1,0,1);
-    public static final ConstColor Red = new Color(1,0,0,1);
-    public static final ConstColor Black = new Color(0,0,0,1);
-    public static final ConstColor White = new Color(1,1,1,1);
-    public static final ConstColor Yellow = new Color(1,1,0,1);
-    public static final ConstColor Magenta = new Color(1,0,1,1);
-    public static final ConstColor Cyan = new Color(0,1,1,1);
+    public static final ConstColor Blue = new Color(0,0,255,255);
+    public static final ConstColor Green = new Color(0,255,0,255);
+    public static final ConstColor Red = new Color(255,0,0,255);
+    public static final ConstColor Black = new Color(0,0,0,255);
+    public static final ConstColor White = new Color(255,255,255,1);
+    public static final ConstColor Yellow = new Color(255,255,0,255);
+    public static final ConstColor Magenta = new Color(255,0,255,255);
+    public static final ConstColor Cyan = new Color(0,255,255,255);
     public static final ConstColor Transparent = new Color(0,0,0,0);
 
 
